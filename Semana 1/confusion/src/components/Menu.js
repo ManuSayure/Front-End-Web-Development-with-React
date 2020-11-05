0182296f2ae10a113c0a147a8fc1480529b12783
Manoela Cassia <manoelacs@ic.ufal.br>
2020-11-05 15:29:36 -0300

    Single Page Aplication Part 2

--- Semana 1/confusion/src/components/Menu.js
+++ Semana 1/confusion/src/components/Menu.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from "react-router-dom";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 
-function RenderMenuItem({dish}) {
+function RenderMenuItem({dish, onClick}) {
     return(
         <div  className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>   
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>                      
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>   
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>  
+                </Link>                    
             </Card>  
         </div>       
     );
@@ -28,8 +31,20 @@ const Menu = (props) =>{
                 
                 
         });
-        return(            
-            <div className= "container" >         
+        return( 
+
+            <div className= "container"> 
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+                </div>
+
                 <div className="row">
                     {menu}                    
                 </div>                  
